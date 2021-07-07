ff65035be05b658a5a47cf40519bd6d5465231db
Rob Harris <61884818+robharrisdw@users.noreply.github.com>
2021-07-06 23:21:47 -0500

    more scripts

---
+++ deploy-dwcc-oracle.sh
@@ -0,0 +1,48 @@
+# What
+## Update and use this script to catalog a postgressql database and turn the metadata into RDF (knowledge graph) format
+# Steps
+## Start with this provided script
+## Once you have uploaded to data.world and notified them
+## and the catalog has been enriched and validated
+## we can automate (scheduled or event trigger) the collection of metadata
+## and building of your metadata catalog in data.world
+
+## If running Windows please remove sudo
+
+sudo docker run -it --rm \
+--mount type=bind,source="$(pwd)"/dwcc-export,target=/dwcc-output \
+--mount type=bind,source="$(pwd)"/dwcc-export,target=/app/log \
+--mount type=bind,source=“$(pwd)“/jdbcdrivers,target=/usr/src/dwcc-config/lib \
+datadotworld/dwcc:2.36 \
+catalog-oracle -A -i -a <account> -d <database> \
+-n <catalogName> -o "/dwcc-output" -p <port> \
+-r <role> -s <server> -u <user>
+
+# help - https://docs.data.world/en/59261-59392-Connect-to-data.html#UUID-53905695-f3e4-58ce-9ded-4488c717a4ac
+# Create and Executable script to run dwcc - https://docs.data.world/en/59261-59277-Quickstarts-and-tutorials.html#UUID-c22d3138-ae67-050c-30d7-eb1f51888159
+# Run and Validate the script - https://docs.data.world/en/59261-59277-Quickstarts-and-tutorials.html#UUID-6dd410f1-5c3f-d003-bb44-ac39dcaa0c82
+
+# docker commands
+## docker run = Tell docker to run/start
+## -it = name the docker run
+## --rm = remove any mounts when done (Clean up)
+## datadotworld/dwcc:2.36 = use the data.world dwcc v3.36 docker container on dockerhub
+## --mount 1 = map the existing folder /dwcc-output inside the dwcc docker container to the dwcc-export folder inside the folder I executed this script from
+## --mount 2 = map the existing folder /app/log inside the dwcc docker container to the dwcc-export folder inside the folder I executed this script from
+## --mount 3 = map the existing folder /user/src/dwcc-config/lib inside the dwcc docker container to a folder locally containing the jdbc driver 
+
+# dwcc commands
+## catalog-oracle = tell dwcc to catalog a oracle database
+## -A = catalog all the databases the user provided has access to
+## -i = request the user to input the database user's password interactively when run (do not add password to script)
+## -a = data.world orgId - Please add fl-<agency_acronym> and replace the <agency_acronym> with your agency's acronym
+### example fl-dmv
+## -d = name of the database being cataloged
+## -n = name of the catalog.  Could be a specific dataset, topic, domain if using the schema flag.
+### Or what do users request access to?  Postgres Prod or Data Lake or Auto Manufactur Analytics
+### This will be a catalog in the data.world UI and what we will ask you to denote what data is Restricted to Public Disclosure
+### And provide a data dictionary, metrics, or terms associated with this data.
+## -o = output directory of the cataloged database in RDF knowledge graph format - please leave as "/dwcc-output"
+## -p = port of the database server being cataloged
+## -s = the host name of the database server being cataloged
+## -u = The username of a user of the database server being cataloged with access to the data you wish to be cataloged
